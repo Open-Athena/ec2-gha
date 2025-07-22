0d7df63cfaad7794658d23f0050d4bbb6ccf4868
Ryan Williams <ryan.williams@openathena.ai>
2025-07-22 01:37:22 -0400

    `"InstanceInitiatedShutdownBehavior": "terminate"`

--- src/start_aws_gha_runner/start.py
+++ src/start_aws_gha_runner/start.py
@@ -87,6 +87,7 @@ class StartAWS(CreateCloudInstance):
             "MinCount": 1,
             "MaxCount": 1,
             "UserData": self._build_user_data(**user_data_params),
+            "InstanceInitiatedShutdownBehavior": "terminate",
         }
         if self.subnet_id != "":
             params["SubnetId"] = self.subnet_id
